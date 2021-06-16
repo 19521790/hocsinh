c8294dee490c4383a13aa38bba0bef89f41cb9f5
vamirio256 <nguyenquocquan.nbk.9a5@gmail.com>
2021-06-16 11:57:53 +0700

    Up

---
+++ DoAnCNPM/src/main/java/thamso/TuoiToiThieu.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thamso;
+
+/**
+ *
+ * @author nguye
+ */
+public class TuoiToiThieu {
+    public static int TuoiToiThieu(){
+        int tuoiToiThieu = 0;
+        
+        return tuoiToiThieu;
+    }
+}
